c7bd075e1b3314801133ae8aef774e380996cdf1
LazyFoox <ibatupvp1@gmail.comx>
2022-09-24 22:02:52 +0800

    d

--- cogs/bots/help.py
+++ cogs/bots/help.py
@@ -8,7 +8,7 @@ class help(commands.Cog):
     @commands.command()
     async def help(self, ctx):
         embed = discord.Embed(title="Help", description=";help (this is a beta commands. textholder)\n;guildinfo\n;button\n;google\n;selected")
-        await ctx.send(embed)
+        await ctx.send(embed=embed)
 
 async def setup(bot):
     await bot.add_cog(help(bot))
